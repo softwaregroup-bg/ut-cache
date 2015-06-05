9ee5d698143c3c8e97f29a84d68637f0fe40576e
stamen.peev <stamen.peev@softwaregroup-bg.com>
2015-06-05 11:50:32 +0300

    ut-cache rewrites

--- index.js
+++ index.js
@@ -1,51 +1,98 @@
 var levelup = require('levelup');
+var leveldown = require('leveldown');
+var memdown = require('memdown');
+var subLevel = require('level-sublevel');
 var ttl = require('level-ttl');
 var when = require('when');
-var cache = {};
-var bus = {};
+var _ = require('lodash');
+var cache;
+var collections = [];
+
+function getCollection(collection) {
+    if (!collections[collection]) {
+        var db = cache.sublevel(collection);
+        collections[collection] = {
+            get: function(key) {
+                return when.promise(function(resolve, reject) {
+                    if (key) {
+                        db.get(key, function(err, value) {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(value);
+                            }
+                        });
+                    } else { // get all records
+                        var data = [];
+                        db.createReadStream()
+                        .on('data', function(record) {
+                            data.push({key: record.key, value: record.value});
+                        })
+                        .on('error', function(err) {
+                            reject(err);
+                        })
+                        .on('close', function() {
+                            console.log('ut-cache read stream closed');
+                        })
+                        .on('end', function() {
+                            resolve(data);
+                        });
+                    }
+                });
+            },
+            set: function(key, value, ttl) {
+                return when.promise(function(resolve, reject) {
+                    db.put(key, value, function(err) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(value);
+                        }
+                    });
+                });
+            },
+            del: function(key) {
+                return when.promise(function(resolve, reject) {
+                    db.del(key, function(err) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(true);
+                        }
+                    });
+                });
+            }
+        }
+    };
+    return collections[collection];
+}
 
 module.exports = {
-    init: function(c) {
-        var config = c || {
-            connectionString: '/memory',
-            storage: 'memdown',
+    init: function(bus) {
+        var config = _.defaults(bus.config.cache || {}, {
+            'location': '/cache',
+            'storage': 'memory',
             'encoding': 'json',
             'checkFrequency': 1000,
             'defaultTTL': 3600
-        };
-        if (config.storage == 'memdown') {
-            cache = levelup(config.connectionString, {
-                db: require('memdown'),
+        });
+        if (config.storage === 'memory') {
+            cache = subLevel(levelup(config.location, {
+                db: memdown,
                 valueEncoding: config.encoding
-            });
-        } else {
-            // TODO Error handling + Other cache storages
+            }));
+        } else if (config.storage === 'disk') {
+            cache = subLevel(levelup(config.location, {
+                db: leveldown,
+                valueEncoding: config.encoding
+            }));
         }
         ttl(cache, {
             checkFrequency: config.checkFrequency,
             defaultTTL: config.defaultTTL * 1000
         });
     },
-    get: function(key) {
-        return when.promise(function(resolve, reject) {
-            cache.get(key, function(err, value) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(value);
-                }
-            });
-        });
-    },
-    set: function(key, value, ttl) {
-        return when.promise(function(resolve, reject) {
-            cache.put(key, value, function (err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(value);
-                }
-            });
-        }.bind(this));
+    collection: function(collection) {
+        return getCollection(collection);
     }
-}
+};
